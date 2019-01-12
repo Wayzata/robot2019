bc456ad447e366d77f5a8231964339f1f56601fe
Divyam Khatri <divyam.khatri@gmail.com>
2019-01-11 20:18:48 -0600

    drivetrain v1

---
+++ robot2019/src/main/java/frc/robot/DriveTrain.java
@@ -0,0 +1,59 @@
+import edu.wpi.first.wpilibj.Talon;
+
+//4 motors
+// front left motors = 4
+//back left motor = 3
+// front right motor = 5
+// back right motor = 6
+
+public class DriveTrain {
+    private Talon frontLeftMotor;
+    private Talon backLeftMotor;
+    private Talon frontRightMotor;
+    private Talon backRightMotor;
+    private final double speedMultiplier = .5;
+
+    public DriveTrain() {
+        frontLeftMotor = new Talon(4);
+        backLeftMotor = new Talon(3);
+        frontRightMotor = new Talon(5);
+        backRightMotor = new Talon(6);
+
+    }
+
+    public void leftMove(double speed) {
+        /*
+         * if(speed < .05){ speed = 0; }
+         * 
+         */
+        if (speed < 0) {
+
+            frontLeftMotor.set(-1 * (Math.pow(speed, 2) * speedMultiplier));
+            backLeftMotor.set(-1 * (Math.pow(speed, 2) * speedMultiplier));
+
+        } else {
+
+            frontLeftMotor.set(1 * (Math.pow(speed, 2) * speedMultiplier));
+            backLeftMotor.set(1 * (Math.pow(speed, 2) * speedMultiplier));
+
+        }
+    }
+
+    public void rightMove(double speed) {
+        /*
+         * if(speed < .05){ speed = 0; }
+         */
+        if (speed < 0) {
+
+            frontLeftMotor.set(-1 * (Math.pow(speed, 2) * speedMultiplier));
+            backLeftMotor.set(-1 * (Math.pow(speed, 2) * speedMultiplier));
+
+        } else {
+
+            frontLeftMotor.set(1 * (Math.pow(speed, 2) * speedMultiplier));
+            backLeftMotor.set(1 * (Math.pow(speed, 2) * speedMultiplier));
+
+        }
+    }
+
+}
--- robot2019/src/main/java/frc/robot/Robot.java
+++ robot2019/src/main/java/frc/robot/Robot.java
@@ -7,7 +7,9 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Joystick.AxisType;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -23,25 +25,32 @@ public class Robot extends TimedRobot {
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  DriveTrain driveTrain = new DriveTrain();
+  Joystick leftJoystick;
+  Joystick rightJoystick;
 
   /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
+   * This function is run when the robot is first started up and should be used
+   * for any initialization code.
    */
   @Override
   public void robotInit() {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
+
+    leftJoystick = new Joystick(1);
+    rightJoystick = new Joystick(2);
   }
 
   /**
-   * This function is called every robot packet, no matter the mode. Use
-   * this for items like diagnostics that you want ran during disabled,
-   * autonomous, teleoperated and test.
+   * This function is called every robot packet, no matter the mode. Use this for
+   * items like diagnostics that you want ran during disabled, autonomous,
+   * teleoperated and test.
    *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
+   * <p>
+   * This runs after the mode specific periodic functions, but before LiveWindow
+   * and SmartDashboard integrated updating.
    */
   @Override
   public void robotPeriodic() {
@@ -49,14 +58,15 @@ public class Robot extends TimedRobot {
 
   /**
    * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable
-   * chooser code works with the Java SmartDashboard. If you prefer the
-   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString line to get the auto name from the text box below the Gyro
+   * between different autonomous modes using the dashboard. The sendable chooser
+   * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+   * remove all of the chooser code and uncomment the getString line to get the
+   * auto name from the text box below the Gyro
    *
-   * <p>You can add additional auto modes by adding additional comparisons to
-   * the switch structure below with additional strings. If using the
-   * SendableChooser make sure to add them to the chooser code above as well.
+   * <p>
+   * You can add additional auto modes by adding additional comparisons to the
+   * switch structure below with additional strings. If using the SendableChooser
+   * make sure to add them to the chooser code above as well.
    */
   @Override
   public void autonomousInit() {
@@ -71,13 +81,13 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousPeriodic() {
     switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
+    case kCustomAuto:
+      // Put custom auto code here
+      break;
+    case kDefaultAuto:
+    default:
+      // Put default auto code here
+      break;
     }
   }
 
@@ -86,6 +96,10 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void teleopPeriodic() {
+
+    driveTrain.leftMove((leftJoystick.getAxis(AxisType.kY)));
+    driveTrain.rightMove((rightJoystick.getAxis(AxisType.kY)));
+
   }
 
   /**
