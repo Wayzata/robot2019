be12486adf16cf6dc267946390369399d3fcbf79
marcelnerd <31940874+marcelnerd@users.noreply.github.com>
2019-01-31 17:09:52 -0600

    yeet

---
+++ src/main/java/frc/robot/Variables.java
@@ -0,0 +1,44 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+/**
+ * Add your docs here.
+ */
+public class Variables {
+
+    
+    //Align direction buttons
+    int leftButton = 1;
+    int rightButton = 2;
+    int alignButton = 3;
+
+    //Motors
+    double driveLimiter = 0.4;
+    double singleMotorAdj1 = 0; 
+    int fLeftMotor = 6;
+    int bLeftMotor = 3;
+    int fRightMotor = 5;
+    int bRightMotor = 4;
+
+    //Joysticks
+    int joyLeftPort = 0;
+    int joyRightPort = 1;
+
+    //Lights
+    //int centerLight = 4;
+    //int frontLight = 5;
+
+    //Other crap
+
+
+
+
+
+
+}
